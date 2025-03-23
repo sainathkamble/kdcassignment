d2dd9b3eb71c2d3244832ea5b6ff1066e4549377
Sainath Kamble <kamblesainath41@gmail.com>
2025-03-23 20:18:22 +0530

    Completed Assignment

---
+++ frontend/src/api/index.api.js
@@ -0,0 +1,21 @@
+// const backendURL = process.env.REACT_APP_BACKEND_URL;
+const backendURL = "http://localhost:8080";
+
+const SummaryApi = {
+  getAllProducts: {
+    url: `${backendURL}/api/get-products`,
+    method: "GET",
+  },
+
+  addProduct: {
+    url: `${backendURL}/api/add-product`,
+    method: "POST",
+  },
+
+  updateProduct: {
+    url: `${backendURL}/api/update-product/:id`,
+    method: "PUT",
+  },
+};
+
+export default SummaryApi;
